223071f17a94372081bf5add2b7c171a06f5ad99
Sidharthkp <sidharthkannan20@gmail.com>
2022-12-21 17:50:26 +0530

    Commit netflix wednesday basics

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react'
 import './App.css'
 import NavBar from './components/NavBar/NavBar';
 import Banner from './components/Banner/Banner';
+import RowPost from './components/RowPost/RowPost';
 function App() {
   return (
     <div className='App'>
       <NavBar />
       <Banner />
+      <RowPost />
     </div>
   );
 }
--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -5,8 +5,9 @@ function Banner() {
     return (
         <div className="banner">
             <div className='content'>
+                <img className='wednesday' src="https://occ-0-2611-3663.1.nflxso.net/dnm/api/v6/tx1O544a9T7n8Z_G12qaboulQQE/AAAABfCsAW7SAIq26sADYMnCvMlPGtA38HXcJSt3KRnz6sXaVI-pBYp8mNfkcI26cNQFGwCpiicIA9ZwripZ1Au9e1EsOXJZIdqddJRhA4GALLfNV0ax5BZfSfZL1mQY0vm6ue0M4MXjREjOdtzgi-eB3GayauwrYaK7WWfzlQOuoobOjNuCoOCnOA.png?r=50a" alt="" />
                 <h1 className='title'>
-                    Movie name
+                    Wednesday
                 </h1>
                 <div className="banner_buttons">
                     <button className="button">Play</button>
@@ -15,7 +16,7 @@ function Banner() {
                 <h1 className='description'>Wednesday is an American coming-of-age supernatural mystery comedy horror streaming television series based on the character Wednesday Addams by Charles Addams.</h1>
             </div>
             <div className="fade_bottom">
-                
+
             </div>
         </div>
     )
